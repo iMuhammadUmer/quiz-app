f5ac555b11ceb58aca56c109158e5ab13c8fb8ad
Muhammad Umer <mumer1297@gmail.com>
2020-10-03 13:27:15 +0500

    [WIP] header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Quiz App work in progress...</h1>
-    </div>
-  );
+  return <header></header>;
 }
 
 export default App;
