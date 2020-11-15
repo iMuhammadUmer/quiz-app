0afdf25e2b79df786119f1f1cf30d907c7010269
Muhammad Umer <mumer1297@gmail.com>
2020-11-15 21:10:16 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="box">Work in progess progressive Web Application</div>;
+  return <div className="box">Work in progess Progressive Web Application</div>;
 }
 
 export default App;
