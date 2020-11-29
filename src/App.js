ad37e0f566acb900d073646d9bfa26084db87893
Muhammad Umer <mumer1297@gmail.com>
2020-11-29 11:47:52 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="box">Work In Progess PWA(Progressive Web Application)</div>;
+  return <div className="box">Quiz App Work In Progess PWA(Progressive Web Application)</div>;
 }
 
 export default App;
